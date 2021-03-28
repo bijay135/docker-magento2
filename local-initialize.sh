402f9a1ea91c5e6b002e8709ac8d82902a337663
bijay135 <bijay.shah@sastodeal.com>
2021-03-28 18:12:23 +0545

    fixed user name variable when running as sudo & fixed string export to file whitespace

--- local-initialize.sh
+++ local-initialize.sh
@@ -5,8 +5,8 @@ echo "Running local initialize script"
 
 # Create folders in host that will be mounted to docker containers
 echo -e "\nCreating folders in host"
-mkdir -p /home/$USER/html/magento
-mkdir -p /home/$USER/.composer
+mkdir -p /home/$SUDO_USER/html/magento
+mkdir -p /home/$SUDO_USER/.composer
 mkdir -p /var/lib/mysql
 mkdir -p /var/lib/redis
 mkdir -p /usr/share/elasticsearch/data
@@ -14,31 +14,37 @@ echo -e "Created all folders in host"
 
 # Configure proper permissions
 echo -e "\nConfiguring proper permissions"
-usermod -aG www-data $USER
-chown www-data:www-data /home/$USER/html/magento
-chmod 2775 /home/$USER/html/magento
-chown $USER:www-data /home/$USER/.composer
-chmod 2775 /home/$USER/.composer
-chown $USER:$USER /usr/share/elasticsearch/data
+usermod -aG www-data $SUDO_USER
+chown www-data:www-data /home/$SUDO_USER/html/magento
+chmod 2775 /home/$SUDO_USER/html/magento
+chown $SUDO_USER:www-data /home/$SUDO_USER/.composer
+chmod 2775 /home/$SUDO_USER/.composer
+chown $SUDO_USER:$SUDO_USER /usr/share/elasticsearch/data
 echo -e "Proper permissions configured"
 
 # Optimize host for elasticsearch / redis
 echo -e "\nOptimizing host for elasticsearch / redis"
 if ! cat /etc/sysctl.conf | grep -q "Elasticsearch compatibility" ; then
-	echo "
+	cat >> /etc/sysctl.conf <<- EOS
+
 	# Elasticsearch compatibility
-	vm.max_map_count=262144" >> /etc/sysctl.conf
+	vm.max_map_count=262144
+	EOS
 fi
 if ! cat /etc/sysctl.conf | grep -q "Redis compatibility" ; then
-	echo "
+	cat >> /etc/sysctl.conf <<- EOS
+
 	# Redis compatibility
 	net.core.somaxconn=1024
-	vm.overcommit_memory=1" >> /etc/sysctl.conf
+	vm.overcommit_memory=1
+	EOS
 fi
 if [ ! -f "/etc/rc.local" ] ; then
-	echo "
+	cat > /etc/rc.local <<- EOS
+
 	#!/bin/bash
-	echo never > /sys/kernel/mm/transparent_hugepage/enabled" > /etc/rc.local
+	echo never > /sys/kernel/mm/transparent_hugepage/enabled
+	EOS
 	chmod +x /etc/rc.local
 	systemctl start rc-local
 fi
@@ -48,20 +54,22 @@ echo "Host redis / elasticsearch optimized"
 echo -e "\nConfiguring docker-compose enviroment file"
 cp -af .env.dis .env
 sed -i 's/$domain_name/null/g' .env
-sed -i 's/$user/'"$USER"'/g' .env
+sed -i 's/$user/'"$SUDO_USER"'/g' .env
 sed -i 's/$es_java_opts/-Xms256m -Xmx256m/g' .env
 echo "Configured docker-compose enviroment file"
 
 # Configure host environment for frequent commands
 if ! cat /etc/environment | grep -q "Magento stack" ; then
 	echo -e "\nConfiguring host enviroment for frequent commands"
-	echo "
+	cat >> /etc/environment <<- EOS
+
 	# Magento stack
-	magento_stack="docker-compose -f $PWD/docker-compose.yml -f $PWD/docker-compose.local.yml "
+	magento_stack="docker-compose -f $PWD/docker-compose.yml -f $PWD/docker-compose.local.yml"
 	php_magento="docker exec -it -u www-data -w /var/www/html/magento php bin/magento"
 	php_composer="docker exec -it -u www-data -w /var/www/html/magento php bash -ic $@ -- composer"
 	redis_cli="docker exec -it redis redis-cli"
-	rabbitmq_ctl="docker exec -it rabbitmq rabbitmqctl"" >> /etc/environment
+	rabbitmq_ctl="docker exec -it rabbitmq rabbitmqctl"
+	EOS
 	echo "Configured host enviroment"
 else
 	echo -e "\nHost enviroment already configured, skipping"
--- server-initialize.sh
+++ server-initialize.sh
@@ -10,7 +10,7 @@ echo "Running server initialize script"
 # Create folders in host that will be mounted to docker containers
 echo -e "\nCreating folders in host"
 mkdir -p /var/www/html/magento
-mkdir -p /home/$USER/.composer
+mkdir -p /home/$SUDO_USER/.composer
 mkdir -p /var/lib/mysql
 mkdir -p /var/lib/redis
 mkdir -p /usr/share/elasticsearch/data
@@ -19,31 +19,37 @@ echo -e "Created all folders in host"
 
 # Configure proper permissions
 echo -e "\nConfiguring proper permissions"
-usermod -aG www-data $USER
+usermod -aG www-data $SUDO_USER
 chown www-data:www-data /var/www/html/magento
 chmod 2775 /var/www/html/magento
-chown $USER:www-data /home/$USER/.composer
-chmod 2775 /home/$USER/.composer
-chown $USER:$USER /usr/share/elasticsearch/data
+chown $SUDO_USER:www-data /home/$SUDO_USER/.composer
+chmod 2775 /home/$SUDO_USER/.composer
+chown $SUDO_USER:$SUDO_USER /usr/share/elasticsearch/data
 echo -e "Proper permissions configured"
 
 # Optimize host for elasticsearch / redis
 echo -e "\nOptimizing host for elasticsearch / redis"
 if ! cat /etc/sysctl.conf | grep -q "Elasticsearch compatibility" ; then
-	echo "
+	cat >> /etc/sysctl.conf <<- EOS
+
 	# Elasticsearch compatibility
-	vm.max_map_count=262144" >> /etc/sysctl.conf
+	vm.max_map_count=262144
+	EOS
 fi
 if ! cat /etc/sysctl.conf | grep -q "Redis compatibility" ; then
-	echo "
+	cat >> /etc/sysctl.conf <<- EOS
+
 	# Redis compatibility
 	net.core.somaxconn=1024
-	vm.overcommit_memory=1" >> /etc/sysctl.conf
+	vm.overcommit_memory=1
+	EOS
 fi
 if [ ! -f "/etc/rc.local" ] ; then
-	echo "
+	cat > /etc/rc.local <<- EOS
+
 	#!/bin/bash
-	echo never > /sys/kernel/mm/transparent_hugepage/enabled" > /etc/rc.local
+	echo never > /sys/kernel/mm/transparent_hugepage/enabled
+	EOS
 	chmod +x /etc/rc.local
 	systemctl start rc-local
 fi
@@ -53,20 +59,22 @@ echo "Host redis / elasticsearch optimized"
 echo -e "\nConfiguring docker-compose enviroment file"
 cp -af .env.dis .env
 sed -i 's/$domain_name/'"$DOMAIN_NAME"'/g' .env
-sed -i 's/$user/'"$USER"'/g' .env
+sed -i 's/$user/'"$SUDO_USER"'/g' .env
 sed -i 's/$es_java_opts/-Xms1g -Xmx1g/g' .env
 echo "Configured docker-compose enviroment file"
 
 # Configure host environment for frequent commands
 if ! cat /etc/environment | grep -q "Magento stack" ; then
 	echo -e "\nConfiguring host enviroment for frequent commands"
-	echo "
+	cat >> /etc/environment <<- EOS
+
 	# Magento stack
-	magento_stack="docker-compose -f $PWD/docker-compose.yml -f $PWD/docker-compose.server.yml "
+	magento_stack="docker-compose -f $PWD/docker-compose.yml -f $PWD/docker-compose.server.yml"
 	php_magento="docker exec -it -u www-data -w /var/www/html/magento php bin/magento"
 	php_composer="docker exec -it -u www-data -w /var/www/html/magento php bash -ic $@ -- composer"
 	redis_cli="docker exec -it redis redis-cli"
-	rabbitmq_ctl="docker exec -it rabbitmq rabbitmqctl"" >> /etc/environment
+	rabbitmq_ctl="docker exec -it rabbitmq rabbitmqctl"
+	EOS
 	echo "Configured host enviroment"
 else
 	echo -e "\nHost enviroment already configured, skipping"
